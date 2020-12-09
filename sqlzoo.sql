50bd1b382d191c1210909d77f6590030b2effbc0
Angel Barros <albarros1973@gmail.com>
2020-12-09 11:29:03 +0100

    Section SELECT from Nobel

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -33,6 +33,21 @@ SELECT name, ROUND(population / 1000000, 2), ROUND(gdp / 1000000000, 2) FROM wor
 SELECT name, ROUND(gdp / population / 1000, 0) * 1000 FROM world WHERE gdp > 1000000000000;
 SELECT name, capital FROM world WHERE LENGTH(name) = LENGTH(capital);
 SELECT name, capital FROM world WHERE LEFT(name,1) = LEFT(capital, 1) AND name != capital;
-SELECT name FROM world WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%'
-  and name LIKE '%u%'
-  and name NOT LIKE '% %'
+SELECT name FROM world WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %';
+
+-- 3 SELECT from Nobel
+SELECT yr, subject, winner FROM nobel WHERE yr = 1950;
+SELECT winner FROM nobel WHERE yr = 1962 AND subject = 'Literature';
+SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein';
+SELECT winner FROM nobel WHERE yr >= 2000 AND subject = 'Peace';
+SELECT yr, subject, winner FROM nobel WHERE yr >= 1980 AND yr <= 1989 AND subject = 'Literature';
+SELECT * FROM nobel WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
+SELECT winner FROM nobel WHERE winner LIKE 'John%';
+SELECT yr, subject, winner FROM nobel WHERE (yr = 1980 AND subject = 'Physics') OR (yr = 1984 AND subject = 'Chemistry');
+SELECT yr, subject, winner FROM nobel WHERE yr = 1980 AND subject != 'Chemistry' AND subject != 'Medicine';
+SELECT yr, subject, winner FROM nobel WHERE (yr < 1910 AND subject = 'Medicine') OR (yr >= 2004 AND subject = 'Literature');
+SELECT yr, subject, winner FROM nobel WHERE winner = 'PETER GRÜNBERG';
+SELECT yr, subject, winner FROM nobel WHERE winner = 'EUGENE O\'NEILL';
+SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner ASC;
+SELECT winner, subject FROM nobel WHERE yr=1984 ORDER BY subject IN ('Physics','Chemistry'), subject, winner;
+
