177e99e59652a9446556bf836c796202acc78885
Angel Barros <albarros1973@gmail.com>
2020-12-10 12:40:13 +0100

    Section COVID 19

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -146,3 +146,14 @@ SELECT constituency, party FROM (SELECT *, RANK() OVER (PARTITION BY constituenc
   WHERE constituency BETWEEN 'S14000021' AND 'S14000026' AND yr  = 2017 AND posn = 1 ORDER BY constituency;
 SELECT party, COUNT(1) FROM (SELECT *, RANK() OVER (PARTITION BY constituency ORDER BY votes DESC) as posn FROM ge WHERE constituency LIKE 'S%' AND yr  = 2017) t
  WHERE constituency LIKE 'S%' AND yr  = 2017 AND posn = 1 GROUP BY party ORDER BY party;
+
+-- 9+ COVID 19
+SELECT name, DAY(whn), confirmed, deaths, recovered FROM covid WHERE name = 'Spain' AND MONTH(whn) = 3 ORDER BY whn;
+SELECT name, DAY(whn), confirmed, LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn) FROM covid WHERE name = 'Italy' AND MONTH(whn) = 3 ORDER BY whn;
+SELECT name, DAY(whn), confirmed - LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn) FROM covid WHERE name = 'Italy' AND MONTH(whn) = 3 ORDER BY whn;
+SELECT name, DATE_FORMAT(whn,'%Y-%m-%d'), confirmed - LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn) FROM covid WHERE name = 'Italy' AND WEEKDAY(whn) = 0 ORDER BY whn;
+SELECT tw.name, DATE_FORMAT(tw.whn,'%Y-%m-%d'), tw.confirmed - lw.confirmed FROM covid tw LEFT JOIN covid lw ON DATE_ADD(lw.whn, INTERVAL 1 WEEK) = tw.whn AND tw.name=lw.name
+  WHERE tw.name = 'Italy' AND WEEKDAY(tw.whn) = 0 ORDER BY tw.whn;
+SELECT name, confirmed, RANK() OVER (ORDER BY confirmed DESC) rc, deaths, RANK() OVER (ORDER BY deaths DESC) rd FROM covid WHERE whn = '2020-04-20' ORDER BY confirmed DESC;
+SELECT world.name, ROUND(100000*confirmed/population,0), RANK() OVER (ORDER BY ROUND(10000*confirmed/population,0) ASC) AS rank FROM covid JOIN world ON covid.name=world.name
+  WHERE whn = '2020-04-20' AND population > 10000000 ORDER BY population DESC;
